ab9f709944af6b448e8d9843687b2999fd489b24
MacBook <macbook@MacBook-Pro-MacBook.local>
2023-06-22 00:33:57 +0200

    refactor: destructuring assign in render

--- scripts/modules/render.js
+++ scripts/modules/render.js
@@ -1,11 +1,27 @@
-import { getCurrentDateTime, getWindDirection } from "./utils.js";  
+import {
+    getCurrentDateTime,
+    getWindDirection
+} from "./utils.js";
 
-export const renderWidgetToday = (widget, data) => { 
-    const {dateFormat, timeFormat, weekdayFormat} = getCurrentDateTime();
-    console.log(data)
+export const renderWidgetToday = (widget, data) => {
+    const {
+        dateFormat,
+        timeFormat,
+        weekdayFormat
+    } = getCurrentDateTime();
+    const {
+        weather: [{
+            icon
+        }],
+        name,
+        main: {
+            temp,
+            feels_like
+        }
+    } = data;
 
     widget.insertAdjacentHTML(
-        'beforeEnd', 
+        'beforeEnd',
         `<div class="widget__today">
             <div class="widget__date-block">
                 <p class="widget__date">${dateFormat}</p>   
@@ -13,43 +29,48 @@ export const renderWidgetToday = (widget, data) => {
                 <p class="widget__day">${weekdayFormat}</p>  
             </div>
             <div class="widget__icon">
-                <img class="widget__img" src="./icon/${data.weather[0].icon}.svg" alt="Weather">
+                <img class="widget__img" src="./icon/${icon}.svg" alt="Weather">
             </div>
             <div class="widget__wheather">
                 <div class="widget__city">
-                    <p>${data.name}</p>  
+                    <p>${name}</p>  
                     <button class="widget__change-city" aria-label="Change city"></button>
                 </div>
-                <p class="widget__temp-big">${data.main.temp} °C</p> 
+                <p class="widget__temp-big">${temp} °C</p> 
                 <p class="widget__felt">feels-like</p> 
-                <p class="widget__temp-small">${data.main.feels_like} °C</p>
+                <p class="widget__temp-small">${feels_like} °C</p>
             </div>
-        </div>`  
-    ) 
-} 
+        </div>`
+    )
+}
 
 export const renderWidgetOther = (widget, data) => {
-    const directionIcon = getWindDirection(data.wind.deg)
+
+    const {
+        main,
+        wind
+    } = data;
+    const directionIcon = getWindDirection(wind.deg)
 
     widget.insertAdjacentHTML(
         'beforeEnd',
         `<div class="widget__other">
             <div class="widget__wind">
                 <p class="widget__wind-title">Wind</p> 
-                <p class="widget__wind-speed">${data.wind.speed} m/s</p> 
+                <p class="widget__wind-speed">${wind.speed} m/s</p> 
                 <p class="widget__wind-text">${directionIcon}</p> 
             </div>
             <div class="widget__humidity">
                 <p class="widget__humidity-title">Humidity</p> 
-                <p class="widget__humidity-value">${data.main.humidity}</p>
+                <p class="widget__humidity-value">${main.humidity}</p>
                 <p class="widget__humidity-text">Т.Р: -0.2 °C</p>
             </div>
             <div class="widget__pressure">
                 <p class="widget__pressure-title">Pressure</p>
-                <p class="widget__pressure-value">${data.main.pressure}</p>
+                <p class="widget__pressure-value">${main.pressure}</p>
                 <p class="widget__pressure-text">mb</p> 
             </div>
-        </div>` 
+        </div>`
     )
 }
 
@@ -82,11 +103,11 @@ export const renderWidgetForecast = (widget) => {
             <img class="widget__day-img" src="./icon/03d.svg" alt="Погода">
             <p class="widget__day-temp">17.3°/11.2°</p>
             </li>                                   
-        </ul>` 
+        </ul>`
     )
-}  
+}
 
 export const showError = (widget, error) => {
     widget.textContent = error.toString();
-    widget.classList.add('widget_error'); 
+    widget.classList.add('widget_error');
 }
